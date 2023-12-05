7fa71d84513295ba14ab04913a31484749eaef55
Replit user <>
2023-12-05 14:55:37 +0000

    updated sidequest

---
+++ pages/home/bolt.js
@@ -0,0 +1,6 @@
+import React from 'react';
+//import { Image } from 'cross-country';
+import Image from 'next/image';
+import bolt from './bolt.svg';
+
+export const Bolt = () =>  <Image src="/bolt.svg" alt="lightening bolt" width={30} height={30} />;
