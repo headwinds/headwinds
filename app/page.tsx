35c967129dc6bc3094a8599b92732b936c828f4e
Brandon Flowers <brandon.flowers@prenuvo.com>
2024-10-07 16:09:19 -0400

    phoros upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import { Wrapper, HeadwindsHomePage } from "cross-country";
+// TODO: Wrapper is not available in this file
+//import { Wrapper, HeadwindsHomePage } from "cross-country";
 import HeadwindsSidequest from "../components/headwinds-sidequest";
 
 export default function Home() {
   return (
-    <Wrapper customClass="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <HeadwindsSidequest />
-    </Wrapper>
+    </div>
   );
 }
---
+++ components/cross-country.d.ts
@@ -0,0 +1,4 @@
+declare module "cross-country" {
+  // Export everything from the module
+  export * from "cross-country";
+}
