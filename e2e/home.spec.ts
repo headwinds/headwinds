b95787ef3d0c02c9067e4198616fae2f52771795
Brandon Flowers <brandon.flowers@prenuvo.com>
2024-10-07 11:46:53 -0400

    only hosted sites to test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,12 +4,15 @@ import { test, expect } from "@playwright/test";
 // ideally I want to setup a github action to test the production URL while deploying
 // and alert me if the production URL fails the test
 
+/*
+Obviously this won't work in a CI/CD pipeline because the test is running on the local machine
+
 test("has title on localhost", async ({ page }) => {
   await page.goto("http://localhost:3000");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/headwinds/);
-});
+}); */
 
 test("has title", async ({ page }) => {
   await page.goto("https://headwinds.vercel.app/");
