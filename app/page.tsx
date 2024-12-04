5255e53f38e4415ae174e1497915198a38773906
Brandon Flowers <brandon.flowers@prenuvo.com>
2024-12-04 12:29:44 -0500

    found gremlin for types issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import Image from "next/image";
-// TODO: Wrapper is not available in this file
-//import { Wrapper, HeadwindsHomePage } from "cross-country";
+
+import { Column, Cleric, HeadwindsLogo } from "cross-country";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       bento here..
+      <Cleric />
     </div>
   );
 }
