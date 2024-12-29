3a1d85a35a5f79f45386ed419b34908592ca4465
brandon flowers <brandonflowers@gmail.com>
2024-12-29 12:05:21 -0500

    gobolt

--- components/headwinds-sidequest.tsx
+++ components/headwinds-sidequest.tsx
@@ -112,7 +112,7 @@ const HeadwindsSidequest = ({ isLoading = false }) => {
         </Row>*/}
         </Row>
         <Paragraph customStyle={paraStyle}>
-          I{"'"}ve joined <Link url="https://www.prenuvo.com">Prenuvo</Link> as
+          I{"'"}ve joined <Link url="https://www.gobolt.com">GoBolt</Link> as
           a Senior Software Engineer. I 💛 solving complex customer journeys,
           writing automatation tests and crafting React components in eCommerce
           and executive dashboards, rich with metrics and data visualisations.{" "}
